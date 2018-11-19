29a21d4b18bb00ff2f4e8db5bb3e66714b7101dc
Robert Collura <rcollura@gmail.com>
2018-11-19 12:34:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ data "terraform_remote_state" "network" {
     bucket = "rc-remote-state-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
   }
 }
 
