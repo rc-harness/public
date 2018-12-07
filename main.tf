4a65f8c11b30121abf3eca4bdd70f3d951dce4a1
Robert Collura <rcollura@gmail.com>
2018-12-07 01:28:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 variable "region" {}
 variable "ecs_cluster" {}
 variable "capacity" {}
+variable "access_key" {}
+variable "secret_key" {}
 
 provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
 
