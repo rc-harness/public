20c19a6f803a51c9e94fc2d594c5639dcd6f2674
Robert Collura <rcollura@gmail.com>
2019-02-04 21:42:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "child" {
 }
   
 module "s3" {
-  source = "git::https://github.com/rc-harness/terraform.git"
+  source = "git::git@github.com:rc-harness/terraform.git"
 }
 
 resource "aws_ecs_cluster" "test-ecs-cluster" {
